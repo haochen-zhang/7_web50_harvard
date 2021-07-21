7813a3b2d0bfd988a464c4fc5b92a55cdbae68cb
haochen-zhang <haochen_zhang@gsd.harvard.edu>
2021-07-22 00:25:49 +0800

    initial commit

---
+++ pset_1/wiki/encyclopedia/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("<str:title>", views.entry, name="entry"),
+    path("search/", views.search, name="search"),
+    path("create/", views.create, name="create"),
+    path("", views.rand, name="rand")
+]
---
+++ pset_1/wiki/encyclopedia/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def entry(request, title):
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "entry_content": util.get_entry(title)
+    })
+
+def search(request):
+    title = request.POST.get('q')
+    if title in util.list_entries():
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "entry_content": util.get_entry(title),
+            "entry_list" : util.list_entries()
+        })
+    else:
+        entries = []
+        for e in util.list_entries():
+            if title in e:
+                entries.append(e)
+        return render(request, "encyclopedia/search_result.html", {
+            "entries": entries
+        })
+
+def create(request):
+    return render(request, "encyclopedia/create.html")
+
+def rand(request):
+    pass
